2681050e523dd4a0312582a456ba5f9fe2c59805
Your Name <yjcho2912@gmail.com>
2022-12-01 14:34:39 +0900

    update solution

--- Level 1/16. 부족한 금액 계산하기.js
+++ Level 1/16. 부족한 금액 계산하기.js
@@ -1,18 +1,17 @@
-// ❗️ 정답코드 아님. 추후 수정 ❗️
-
 function solution(price, money, count) {
     let answer = -1;
     let total_pay = 0; // 총 필요한 이용금액
-    let n = 1;
+    let temp_pay = 0;
     
-    // price번 반복
+    // price를 count번 반복 (count번 이용)
+    // 그리고 그 결과값을 temp_pay에 담기
     for(let i = 1; i <= count; i++) {
-        n = n * i;
-        total_pay += n;
-        console.log(total_pay);
+        temp_pay = price * i; // price값은 고정
+        total_pay += temp_pay;
     }
     
-    // total_pay - money = result
+    // 총 금액이 money보다 작은 경우, 0 return
+    (total_pay < money) ? answer = 0 : answer = total_pay - money;
 
     return answer;
 }
